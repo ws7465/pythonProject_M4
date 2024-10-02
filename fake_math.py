1dfa82a5c3fbe4f27ec04872bb420472d0a32abf
ws7465 <ws7465@ya.ru>
2024-10-02 11:44:22 +0300

    module_4.1 20241002 11:43

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second) :  #
+    if second == 0 :  #
+        return "ошибка" #
+    else :  #
+        return first / second  #
+
---
+++ true_math.py
@@ -0,0 +1,6 @@
+def divide(first, second) :  #
+    if second == 0 :  #
+        from math import inf  #
+        return inf #
+    else :  #
+        return first / second  #
