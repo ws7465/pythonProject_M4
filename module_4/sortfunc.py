b6836eadca6f3bd0972ce39aa923b6eeca5f83b3
ws7465 <ws7465@ya.ru>
2024-10-08 12:02:13 +0300

    module_4 20241008 12:02 - sortfunc

--- module_4/sortfunc.py
+++ module_4/sortfunc.py
@@ -1,10 +1,9 @@
 # СОРТИРОВКИ
 #
 #
-nums = [9, 7, 6, 5, 3, 2, 1, 8, 0, 4, 1, 11]
+nums = [9, 7, 6, 5, 3, 2, 1, 8, 0, 4, 1, 11, 23, 5, 456]
 #
 # 1. Пузырьком
-#
 def bubble_sort(ls) :  #
     swapped = True  # если swapped = False , значит уже отсортировано (в последнем цикле не было перестановок)
     while swapped :  # цикл "пока swapped = True". Пока есть перестановки
@@ -13,10 +12,13 @@ def bubble_sort(ls) :  #
             if ls[i] > ls[i + 1] : #
                 ls[i], ls[i + 1] = ls[i + 1], ls[i]  #
                 swapped = True  # была перестановка (ставим "флаг")
+    return ls
 #
-#print(bubble_sort(nums)) # ТАК - ничего не будет, выдаст NONE т.к. print сработает до того, как заполнится массив nume
-bubble_sort(nums)  # заполнение сортированного массива
-print(nums)
+#print(bubble_sort(nums)) # если в ф-ции не будет return ls , ТАК - ничего не будет,
+#              выдаст NONE т.к. print сработает до того, как заполнится массив nume
+#bubble_sort(nums)  # заполнение сортированного массива
+#print(nums)
+print(bubble_sort(nums)) #
 #
 #
 # 2. Выборкой
@@ -27,9 +29,26 @@ def selection_sort(ls) :  #
             if ls[j] < ls[lowest]:  # если какой то элемент меньше наименьшего,
                 lowest = j  # то наименьшим становится он.
         ls[i], ls[lowest] = ls[lowest], ls[i]  #
+    return ls
 #
-selection_sort(nums)  # заполнение сортированного массива
-print(nums)
+#selection_sort(nums)  # заполнение сортированного массива
+#print(nums)
+print(selection_sort(nums)) #
+#
+#
+# 3. Вставкой
+def insertion_sort(ls) :  #
+    for i in range(1, len(ls)) : # перебор списка, начиная с 2-го элемента
+        key = ls[i]  #
+        j = i - 1
+        while ls[j] > key and j >= 0 :  #
+            ls[j + 1] = ls[j]  #
+            j -= j  #
+        ls[j + 1] = key  #
+    return ls
+#
+print(insertion_sort(nums)) #
+#
 #
 #  конец задания
 #
