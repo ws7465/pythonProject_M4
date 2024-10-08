e95ccf756b50b7b497336165be9db5f55f8a9f60
ws7465 <ws7465@ya.ru>
2024-10-08 17:32:41 +0300

    module_4 20241008 17:32 - sortfunc

--- module_4/sortfunc.py
+++ module_4/sortfunc.py
@@ -36,19 +36,22 @@ def selection_sort(ls) :  #
 print(selection_sort(nums)) #
 #
 #
-# 3. Вставкой
-def insertion_sort(ls) :  #
-    for i in range(1, len(ls)) : # перебор списка, начиная с 2-го элемента
-        key = ls[i]  #
-        j = i - 1
-        while ls[j] > key and j >= 0 :  #
-            ls[j + 1] = ls[j]  #
-            j -= j  #
-        ls[j + 1] = key  #
-    return ls
-#
-print(insertion_sort(nums)) #
-#
+# # 3. Вставкой  - АЛГОРИТМ - ГОВНО !!
+# def insertion_sort(ls) :  #
+#     for i in range(1, len(ls)) : # перебор списка, начиная с 2-го элемента
+#         key = ls[i]  #
+#         j = i - 1
+#         #print(locals())  #
+#         # while ls[j] > key and j >= 0 :  #
+#         #     ls[j + 1] = ls[j]  #
+#         #     print(locals())  #
+#         #     j -= j  #
+#         ls[j + 1] = key  #
+#         #print(locals())  #
+#     return ls
+# #
+# print(insertion_sort(nums)) #
+# #
 #
 #  конец задания
 #
